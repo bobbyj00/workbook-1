bd765197cb273e9c3bb76480dea0e0af219cd04b
Bobby Johnson <bobbyj122703@gmail.com>
2025-04-10 11:25:54 -0500

    Completed ShoppingList app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args) {
+        System.out.println("Apples");
+        System.out.println("Bananas");
+        System.out.println("Strawberries");
+        System.out.println("Broccoli");
+        System.out.println("Oranges");
+        System.out.println("Carrots");
+        System.out.println("Water");
+        System.out.println("Plates");
+        System.out.println("Toilet Paper");
+        System.out.println("Dog Food");
+    }
+
+}
